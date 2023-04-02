06f3d0f5ae7de7688fdff5b60e581050472d9364
Filip7243 <filip7243@gmail.com>
2023-04-02 11:05:10 +0200

    target classes and main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Board b = new Board();
-        Character[][] board = b.getBoard();
-
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                System.out.println(board[i][j]);
-            }
-        }
+        Game g = new Game(new Board());
     }
 }
