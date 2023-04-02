04d0894aa101a03454050bd277405ecc6969941f
Filip7243 <filip7243@gmail.com>
2023-04-02 11:04:27 +0200

    added playable interface for borad

--- src/main/java/org/example/Playable.java
+++ src/main/java/org/example/Playable.java
@@ -1,2 +1,9 @@
-package org.example;public interface Playable {
+package org.example;
+
+public interface Playable {
+
+    void makeMove(int x, int y);
+    boolean isMarkPlaced(int x, int y);
+    boolean isGameOver();
+
 }
