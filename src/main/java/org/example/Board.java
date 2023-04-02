ad8d3c0b421a3de845843c02788686b4bc3d852c
Filip7243 <filip7243@gmail.com>
2023-04-02 11:03:45 +0200

    changed field name

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -3,13 +3,13 @@ package org.example;
 public class Board implements Playable {
 
     private final BoardCurrentStateChecker STATE_CHECKER = new BoardCurrentStateChecker(this);
-    private static final Character[][] BOARD = new Character[4][4];
+    private static final Character[][] GAME_BOARD = new Character[4][4];
     private static final char MARK = 'X';
 
     @Override
     public void makeMove(int x, int y) {
         if (!isMarkPlaced(x, y) && !isGameOver()) {
-            BOARD[x][y] = MARK;
+            GAME_BOARD[x][y] = MARK;
         } else {
             throw new IllegalArgumentException("Illegal move! " + x + " " + y);
         }
@@ -17,7 +17,7 @@ public class Board implements Playable {
 
     @Override
     public boolean isMarkPlaced(int x, int y) {
-        return BOARD[x][y] != null;
+        return GAME_BOARD[x][y] != null;
     }
 
     @Override
@@ -27,18 +27,18 @@ public class Board implements Playable {
 
     public Character[][] getBoard() {
         Character[][] boardCopy = new Character[4][4];
-        System.arraycopy(BOARD, 0, boardCopy, 0, 4);
+        System.arraycopy(GAME_BOARD, 0, boardCopy, 0, 4);
 
         return boardCopy;
     }
 
     public void printBoard() {
         System.out.print("   1   2   3   4 \n");
-        for (int i = 0; i < BOARD.length; i++) {
+        for (int i = 0; i < GAME_BOARD.length; i++) {
             System.out.print(i + 1);
 
-            for (int j = 0; j < BOARD[i].length; j++) {
-                String markOnCurrentPosition = BOARD[i][j] == null ? " " : BOARD[i][j].toString();
+            for (int j = 0; j < GAME_BOARD[i].length; j++) {
+                String markOnCurrentPosition = GAME_BOARD[i][j] == null ? " " : GAME_BOARD[i][j].toString();
                 System.out.print(" [" + markOnCurrentPosition + "]");
             }
             System.out.println();
