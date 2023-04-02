aabcc5634af24c4bd7f00958e32b57e862dbc579
Filip7243 <filip7243@gmail.com>
2023-04-02 12:42:37 +0200

    added computer to game

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class Game {
 
     private Board board;
+    private Computer computer;
     private static Scanner scanner = new Scanner(System.in);
 
     public Game(Board board) {
         this.board = board;
-
+        this.computer = new Computer();
         play();
     }
 
@@ -20,15 +21,23 @@ public class Game {
             try {
                 if (!board.isGameOver()) {
                     board.printBoard();
+                    int[] bestMove = computer.findBestMove(board);
+                    board.makeMove(bestMove[0], bestMove[1]);
+                    board.printBoard();
+
                     System.out.print("Make a move (coords separated by comma): ");
                     String move = scanner.nextLine();
                     String[] moves = move.split(",");
                     board.makeMove(Integer.parseInt(moves[0]), Integer.parseInt(moves[1]));
+
+
                 } else {
+                    board.printBoard();
                     flag = false;
                     System.out.println("GAME OVER!");
                 }
             } catch (IllegalArgumentException e) {
+                board.printBoard();
                 flag = false;
             }
         }
