45276af5104924331e28aab8c48f5b31b07d8126
Filip7243 <filip7243@gmail.com>
2023-04-02 11:04:11 +0200

    created simple game class

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,2 +1,36 @@
-package org.example;public class Game {
+package org.example;
+
+import java.util.Scanner;
+
+public class Game {
+
+    private Board board;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public Game(Board board) {
+        this.board = board;
+
+        play();
+    }
+
+    private void play() {
+        boolean flag = true;
+
+        while (flag) {
+            try {
+                if (!board.isGameOver()) {
+                    board.printBoard();
+                    System.out.print("Make a move (coords separated by comma): ");
+                    String move = scanner.nextLine();
+                    String[] moves = move.split(",");
+                    board.makeMove(Integer.parseInt(moves[0]), Integer.parseInt(moves[1]));
+                } else {
+                    flag = false;
+                    System.out.println("GAME OVER!");
+                }
+            } catch (IllegalArgumentException e) {
+                flag = false;
+            }
+        }
+    }
 }
