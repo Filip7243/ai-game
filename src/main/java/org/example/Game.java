1b3d06d602ddec8792e9bae1d362eb5a5ef482e0
Filip <filip7243@gmail.com>
2023-04-07 20:47:34 +0200

    refator code

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -7,6 +7,7 @@ public class Game {
     private Board board;
     private Computer computer;
     private static Scanner scanner = new Scanner(System.in);
+    private BoardCurrentStateChecker bc;
 
     public Game(Board board) {
         this.board = board;
@@ -18,28 +19,33 @@ public class Game {
         boolean flag = true;
 
         while (flag) {
-            try {
-                if (!board.isGameOver()) {
+            if (!board.isGameOver()) {
+                int[] bestMove = computer.findBestMove(board);
+                board.makeMove(bestMove[0], bestMove[1]);
+                System.out.println(computer.getCurrentStateOfBoard(board));
+                if (board.isGameOver()) {
+                    System.out.println("ESSUNIA ZIOMEK KOMP (i, j) = " + bestMove[0] + " " + bestMove[1]);
                     board.printBoard();
-                    int[] bestMove = computer.findBestMove(board);
-                    board.makeMove(bestMove[0], bestMove[1]);
-                    board.printBoard();
-
-                    System.out.print("Make a move (coords separated by comma): ");
-                    String move = scanner.nextLine();
-                    String[] moves = move.split(",");
-                    board.makeMove(Integer.parseInt(moves[0]), Integer.parseInt(moves[1]));
-
-
-                } else {
-                    board.printBoard();
-                    flag = false;
-                    System.out.println("GAME OVER!");
+                    break;
                 }
-            } catch (IllegalArgumentException e) {
+                board.printBoard();
+
+                System.out.print("Make a move (coords separated by comma): ");
+                String move = scanner.nextLine();
+                String[] moves = move.split(",");
+                board.makeMove(Integer.parseInt(moves[0]), Integer.parseInt(moves[1]));
+                if (board.isGameOver()) {
+                    System.out.println("ESSUNIA ZIOMEK PLAYER");
+                    board.printBoard();
+                    break;
+                }
+
+            } else {
                 board.printBoard();
                 flag = false;
+                System.out.println("GAME OVER!");
             }
+
         }
     }
 }
